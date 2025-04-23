d9b3d85ad2d7353024e3f0a9673ce735d44e8d6a
Wisey <okun5315455@gmail.com>
2025-04-23 16:29:59 +0200

    i know i said im gonna work on the side bar ,but i thought i'd be better if i finish the logic for adding to dos , and then i thought well might as well setup the ui for it as well, so i made an array to store all the todos in ,and made an event listener for the add todo button, gave it a modal with the dialog element,added the html elements for it and styles yada yada ...

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import './styles.css';
+import { main } from './sidebar.js'; //importing the main content
 
 const todos = []; //array to store all todos
+const addbtn = document.querySelector('.add-todo-btn');
+//modal for the add button
+const modal = document.createElement('dialog');
+modal.classList.add('modal');
+
 
 //constructor for the to-do list
 class TodoList {
@@ -31,7 +37,49 @@ function getAllTodos() {
 const todoList1 = new TodoList("Buy groceries", "Milk, Bread, Eggs", "2023-10-01", "High", false);
 const todoList2 = new TodoList("Clean the house", "Living room, Kitchen", "2023-10-02", "Medium", false);
 
-console.log(getAllTodos()); 
+console.log(getAllTodos());
+
+
+//event listener for the add button to create a 
+// modal for inputing the data for the todo
+addbtn.addEventListener('click', function () {
+    main.appendChild(modal);
+    modal.showModal();
+    modal.innerHTML = `
+    
+    <button class="modal-close-btn">&times;</button>
+    <form method="dialog">
+        <h2>Add Todo</h2>
+        <label for="title">Title:</label>
+        <input type="text" id="title" required>
+        <label for="description">Description:</label>
+        <textarea id="description" required></textarea>
+        <label for="date">Date:</label>
+        <input type="date" id="date" required>
+        <label for="priority">Priority:</label>
+        <select id="priority" required>
+            <option value="High">High</option>
+            <option value="Medium">Medium</option>
+            <option value="Low">Low</option>
+        </select>
+        <button class="sbtn" type="submit">Add Todo</button>
+    </form>
+
+
+    `;
+
+    modal.addEventListener('click', function (e) {
+        if (e.target.classList.contains('modal-close-btn')) {
+            modal.close();
+            modal.remove();
+        }
+    });
+})
+    
+
+
+
+
     export default TodoList;
 
 
--- src/sidebar.js
+++ src/sidebar.js
@@ -1,5 +1,5 @@
 const all =document.querySelector(".all");
-const main = document.querySelector(".main-content");
+export const main = document.querySelector(".main-content");
 const title = document.querySelector(".titleh1");
 const description = document.querySelector(".descriptionpara");
 all.addEventListener("click", function () {
